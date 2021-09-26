c066118c2f4d0e2a484769fc63e93bc21cf3b13a
BuildTools <wafaaelayoubi.w@gmail.com>
2021-09-26 20:16:33 +0300

    add slider

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,17 @@
 import { useHistory } from 'react-router-dom';
+import SimpleImageSlider from "react-simple-image-slider";
+import slider2 from "../images/slider2.jpg";
+import slider3 from "../images/slider3.jpg";
+import slider4 from "../images/slider4.jpg";
+import slider5 from "../images/slider5.jpg";
+
+const images = [
+  { url: slider2 },
+  { url: slider3 },
+  { url: slider4 },
+  { url: slider5 },
+];
+
 
 const Home = () => {
 
@@ -6,9 +19,15 @@ const Home = () => {
   let history = useHistory();
 
     return (
-      <div>
-          <h3>home</h3>
-      </div>
+     
+           <SimpleImageSlider
+        width={1400}
+        height={504}
+        images={images}
+        showBullets={true}
+        showNavs={true}
+      />
+      
     )
   }
   
